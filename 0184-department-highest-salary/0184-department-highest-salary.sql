5989baeaa87a60fc43987fe1a52e18405672608f
BETÜL ALBAYRAK <52135580+BetulAlbayrak1999@users.noreply.github.com>
2023-03-15 17:33:28 +0300

    Time: 1134 ms (39.60%), Space: 0B (100.00%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,4 @@
+SELECT dept.name AS Department, emp.name AS Employee, emp.salary AS Salary
+FROM Department dept, Employee emp
+WHERE emp.departmentId = dept.id
+AND emp.salary = (SELECT MAX(Salary) FROM Employee emp WHERE emp.departmentId = dept.id);
