8b56a86d676eaaccecad6c26f40c0f8e7f04b8fb
BETÜL ALBAYRAK <52135580+BetulAlbayrak1999@users.noreply.github.com>
2023-03-17 16:54:06 +0300

    Time: 639 ms (88.71%), Space: 0B (100.00%) - LeetHub

---
+++ 1393-capital-gain-loss/1393-capital-gain-loss.sql
@@ -0,0 +1,8 @@
+/* Write your PL/SQL query statement below */
+
+
+select stock_name, 
+sum(case when operation = 'Buy' then -price
+else price end) capital_gain_loss
+from Stocks
+group by stock_name;
