32a138de7cd94099266ef951abf02ace397871da
BETÜL ALBAYRAK <52135580+BetulAlbayrak1999@users.noreply.github.com>
2023-03-20 21:01:20 +0300

    Time: 939 ms (50.62%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,7 @@
+/* Write your PL/SQL query statement below */
+
+select employee_id, 
+case when MOD(employee_id,2) = 1 and name not like 'M%'
+then salary  else 0 end bonus 
+from Employees
+order by employee_id
